47a75f0392e80043aafc4f39428d395fdc633f12
Dean DeHart <DeHartDean@gmail.com>
2023-09-29 14:52:05 -0400

    Change Mail Previewer to use User.last

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -3,7 +3,7 @@ class DeviseMailerPreview < ActionMailer::Preview
 
   # Accessible from http://localhost:3000/rails/mailers/devise/mailer/confirmation_instructions
   def confirmation_instructions
-    user = User.first
+    user = User.last
     token = user.confirmation_token || "some_temporary_token"
     Devise::Mailer.confirmation_instructions(user, token)
   end
